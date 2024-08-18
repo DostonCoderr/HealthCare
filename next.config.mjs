9c14be31a2a9c21e4c1258d042f243b8fe11b69b
Doston1232696 <otkurovdoston69@gmail.com>
2024-08-18 16:38:13 +0500

    Change Desc

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,8 @@ export default withSentryConfig(withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "javascript-mastery",
-project: "care-pulse",
+org: "DostonCoderr",
+project: "Health Care",
 
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
